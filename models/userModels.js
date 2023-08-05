696c0cfa5e389b733a085fa8267c48f123039662
MRaghebdev <mohamedragheb@geexar.dev>
2023-08-05 11:29:03 +0300

    5/8

--- models/userModels.js
+++ models/userModels.js
@@ -1,10 +1,5 @@
 const joi = require("joi");
 const connection = require("../config/db_data");
-const admin = require("../config/fireBase_config.js");
-// const buckt = admin.storage().bucket();
-console.log(admin)
-const { Storage } = require("@google-cloud/storage");
-const storage = new Storage();
 function UserSignIn(req, res) {
   // declare data
   const { username, password } = req.body;
@@ -29,7 +24,7 @@ function UserSignIn(req, res) {
     });
   } else {
     connection.query(
-      "SELECT id , nickname ,username token ,role ,favPLayGrounds ,friend_req,friends,block,intersting_pg,intersting_users,img,sports_playedF FROM users WHERE UserName = ? && password = ? ",
+      "SELECT id , nickname ,username token ,role ,favPLayGrounds ,friend_req,friends,block,intersting_pg,intersting_users,img,sports_played FROM users WHERE UserName = ? && password = ? ",
       [username, password],
       (error, resualt) => {
         if (error) {
@@ -43,4 +38,47 @@ function UserSignIn(req, res) {
     );
   }
 }
-module.exports = { UserSignIn };
+function UserSignUp(req, res) {
+  // declare data
+  const {
+    username,
+    password,
+    confirmed_password,
+    email,
+    phone,
+    sports_played,
+  } = req.body;
+  // validtion on data
+  const schema = joi.object({
+    username: joi.string().required().min(6).max(16),
+    password: joi.string().required().min(8),
+    confirmed_password: joi.any().valid(joi.ref("password")).required(),
+    email: joi.string().email().required(),
+    phone: joi
+      .string()
+      .pattern(/^[0-9]+$/)
+      .required(),
+  });
+  const err = schema.validate(
+    {
+      username: username,
+      password: password,
+      confirmed_password: confirmed_password,
+      email: email,
+      phone: phone,
+    },
+    {
+      abortEarly: false,
+    }
+  );
+  if (err.error?.details.length > 0) {
+    let errMessage = [];
+    for (let i = 0; i < err.error.details.length; i++) {
+      errMessage.push(err.error.details[i].message);
+    }
+    res.json({
+      Errors: errMessage,
+    });
+  }
+}
+module.exports = { UserSignIn, UserSignUp };
