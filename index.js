51ebfcee81df2a89d2d2de7631d0aea2e54cd0c3
MRaghebdev <modyahmed221@gmail.com>
2023-10-30 23:25:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,32 +4,60 @@ const userRoute = require("./routes/userRoute");
 const sportsRoute = require("./routes/sportsRoute");
 const playGroundRoute = require("./routes/playGroundRoute");
 
-const db = require("./config/db_data");
+const sequelize = require("./config/db_data");
 class CustomError extends Error {
+
   constructor(message, statusCode) {
     super(message);
     this.statusCode = statusCode;
   }
 }
 global.CustomError = CustomError;
-db.sync().then(() => {
-  const app = express();
+// db.sync().then(() => {
+//   const app = express();
+//
+//   app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
+//   app.use("/users", userRoute);
+//   app.use("/sports", sportsRoute);
+//   app.use((err, req, res, next) => {
+//     // Extract the status code and message from the error object
+//     const statusCode = err.statusCode || 500;
+//     const errorMessage = err.message || "Internal Server Error";
+//     res.status(statusCode).json({
+//       message: errorMessage,
+//     });
+//   });
+//   // Start the server
+//   app.listen(3000, () => {
+//     console.log("Server started on port 3000");
+//   });
+// });
+ async function myApp(){
+  try {
+       await sequelize.sync()
+    const app = express();
 
-  app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
-  app.use("/users", userRoute);
-  app.use("/sports", sportsRoute);
-  app.use((err, req, res, next) => {
-    // Extract the status code and message from the error object
-    const statusCode = err.statusCode || 500;
-    const errorMessage = err.message || "Internal Server Error";
-    res.status(statusCode).json({
-      message: errorMessage,
+    app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
+    app.use("/users", userRoute);
+    app.use("/sports", sportsRoute);
+    app.use((err, req, res, next) => {
+      // Extract the status code and message from the error object
+      const statusCode = err.statusCode || 500;
+      const errorMessage = err.message || "Internal Server Error";
+      res.status(statusCode).json({
+        message: errorMessage,
+      });
     });
-  });
-  // Start the server
-  app.listen(3000, () => {
-    console.log("Server started on port 3000");
-   export default app
-  });
-});
+    // Start the server
+    app.listen(3000, () => {
+      console.log("Server started on port 3000");
+    });
+  }catch (error){
+  throw (error)
+  }
+}
+myApp()
+   // export default app
+
+
 
