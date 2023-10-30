cd9cb542fee6006a55ba334cb214219f1bde1591
MRaghebdev <modyahmed221@gmail.com>
2023-10-30 23:41:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,51 +13,51 @@ class CustomError extends Error {
   }
 }
 global.CustomError = CustomError;
-// db.sync().then(() => {
-//   const app = express();
+  const app = express();
+sequelize.sync().then(() => {
+
+  app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
+  app.use("/users", userRoute);
+  app.use("/sports", sportsRoute);
+  app.use((err, req, res, next) => {
+    // Extract the status code and message from the error object
+    const statusCode = err.statusCode || 500;
+    const errorMessage = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+      message: errorMessage,
+    });
+  });
+  /// Start the server
+  app.listen(3000, (err) => {
+    console.log("Server started on port 3000",err);
+  });
+});
+// const app = express();
 //
-//   app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
-//   app.use("/users", userRoute);
-//   app.use("/sports", sportsRoute);
-//   app.use((err, req, res, next) => {
-//     // Extract the status code and message from the error object
-//     const statusCode = err.statusCode || 500;
-//     const errorMessage = err.message || "Internal Server Error";
-//     res.status(statusCode).json({
-//       message: errorMessage,
+//  async function myApp(){
+//   try {
+//        await sequelize.sync()
+//
+//     app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
+//     app.use("/users", userRoute);
+//     app.use("/sports", sportsRoute);
+//     app.use((err, req, res, next) => {
+//       // Extract the status code and message from the error object
+//       const statusCode = err.statusCode || 500;
+//       const errorMessage = err.message || "Internal Server Error";
+//       res.status(statusCode).json({
+//         message: errorMessage,
+//       });
 //     });
-//   });
-//   // Start the server
-//   app.listen(3000, () => {
-//     console.log("Server started on port 3000");
-//   });
-// });
-const app = express();
+//     // Start the server
+//     app.listen(3000, (err) => {
+//       console.log("Server started on port 3000");
+//     });
+//   }catch (error){
+//   console.log(error)
+//   }
+// }
+// myApp()
 
- async function myApp(){
-  try {
-       await sequelize.sync()
 
-    app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
-    app.use("/users", userRoute);
-    app.use("/sports", sportsRoute);
-    app.use((err, req, res, next) => {
-      // Extract the status code and message from the error object
-      const statusCode = err.statusCode || 500;
-      const errorMessage = err.message || "Internal Server Error";
-      res.status(statusCode).json({
-        message: errorMessage,
-      });
-    });
-    // Start the server
-    app.listen(3000, (err) => {
-      console.log("Server started on port 3000");
-    });
-  }catch (error){
-  console.log(error)
-  }
-}
-myApp()
 module.exports = app
-
-
