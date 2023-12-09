9a667492fd0148d15ef28f63f22ecffd3ec9c395
Mohamed Ahmed Ragheb <modyahmed221@gmail.com>
2023-12-09 22:47:34 +0200

    test

--- handelers/otpUtils.js
+++ handelers/otpUtils.js
@@ -1,12 +1,9 @@
-
-
-const otpGenerator = require('otp-generator');
-function  createOtpCode(){
-return otpGenerator.generate(6,{
+const otpGenerator = require('otp-generator')
+function createOtpCode() {
+  return otpGenerator.generate(6, {
     upperCaseAlphabets: false,
     lowerCaseAlphabets: false,
     specialChars: false,
-})
-
+  })
 }
-module.exports = {createOtpCode}
+module.exports = createOtpCode
