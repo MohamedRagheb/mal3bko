82acddc7075e9d87680c8e7339f1775ab5111264
Mohamed Ahmed Ragheb <modyahmed221@gmail.com>
2023-12-09 23:00:07 +0200

    hanele

--- services/emailService/index.js
+++ services/emailService/index.js
@@ -1,26 +1,25 @@
-const nodemailer = require('nodemailer');
-const verfiyEmailTempelate = require("./emailTempelets/VerifyEmailTempelate")
-async function sendVerificationEmail(userEmail,code,userName){
-    
-        const transporter = nodemailer.createTransport({
-            service: 'Gmail', // Use the email service provider you want
-            auth: {
-                user: process.env.EMAIL_USERNAME,
-                pass: process.env.EMAIL_password,
-            },
-        });
-        const mailOptions = {
-            from: process.env.EMAIL_USERNAME,
-            to: userEmail,
-            subject: `mal3bko Email verification code`,
-            html: verfiyEmailTempelate(code,userName)
-        };
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error('Error sending email:', error);
-            } else {
-                console.log('Email sent:', info.response);
-            }});
-
+const nodemailer = require('nodemailer')
+const verfiyEmailTempelate = require('./emailTempelets/VerifyEmailTempelate')
+const sendVerificationEmail = async (userEmail, code, userName) => {
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail', // Use the email service provider you want
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_password,
+    },
+  })
+  const mailOptions = {
+    from: process.env.EMAIL_USERNAME,
+    to: userEmail,
+    subject: `mal3bko Email verification code`,
+    html: verfiyEmailTempelate(code, userName),
+  }
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error)
+    } else {
+      console.log('Email sent:', info.response)
+    }
+  })
 }
-    module.exports = sendVerificationEmail
+module.exports = sendVerificationEmail
